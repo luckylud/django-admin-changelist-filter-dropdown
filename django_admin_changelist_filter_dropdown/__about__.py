73fa212a6690fb0228976c8a554c88b94c17a5da
Ludovic Watteaux <luckylud@gmail.com>
2019-04-07 17:13:31 +0200

    Release 1.0.2: over behavior now done by css instead of jquery

--- django_admin_changelist_filter_dropdown/__about__.py
+++ django_admin_changelist_filter_dropdown/__about__.py
@@ -14,7 +14,7 @@ __summary__ = "Make django admin changelist-filter div collapsable "\
 
 __url__ = 'https://github.com/luckylud/django-admin-changelist-filter-dropdown'
 
-__version_info__ = (1, 0, '1')
+__version_info__ = (1, 0, '2')
 __version__ = '.'.join(map(str, __version_info__))
 
 __author__ = 'Ludovic Watteaux'
